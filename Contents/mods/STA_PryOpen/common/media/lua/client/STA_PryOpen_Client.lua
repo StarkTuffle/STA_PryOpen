0b6242d123db3829b5817ecd39932c3a8ede61d5
StarkTruffle <astarkie-github@odraefys.dev>
2026-05-28 11:25:32 -0600

    Fixed desync issue

--- Contents/mods/STA_PryOpen/common/media/lua/client/STA_PryOpen_Client.lua
+++ Contents/mods/STA_PryOpen/common/media/lua/client/STA_PryOpen_Client.lua
@@ -83,6 +83,7 @@ function Client.onServerCommand(module, command, args)
                         o:ToggleDoor(playerObj)
                     end
                 end
+                o:sync()
             end
         end
     end
